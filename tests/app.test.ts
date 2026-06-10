31a227c9e9c9ba6ac147801d726d44cc108b42d6
Robert <robert.s.forristall@gmail.com>
2026-06-09 22:39:30 -0400

    test(auth): finish auth test integrations

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -2,12 +2,28 @@ import app from "../src/app.js";
 import supertest from "supertest";
 import * as test_db from "./test_db.js";
 import mongoose from "mongoose";
-import { PostgreSqlContainer } from "@testcontainers/postgresql";
+import {
+  PostgreSqlContainer,
+  StartedPostgreSqlContainer,
+} from "@testcontainers/postgresql";
 import { execSync } from "child_process";
+import {
+  describe,
+  test,
+  expect,
+  beforeAll,
+  afterEach,
+  afterAll,
+} from "@jest/globals";
+import { auth } from "../src/auth.js";
+import { TestHelpers } from "better-auth/plugins";
+import TestAgent from "supertest/lib/agent.js";
 
-let request = supertest(app);
+let request: TestAgent;
 
-let container: any;
+let container: StartedPostgreSqlContainer;
+
+let authTest: TestHelpers;
 
 describe("Test request with mongoose", () => {
   beforeAll(async () => {
@@ -16,7 +32,10 @@ describe("Test request with mongoose", () => {
       console.log("Mongoose connected to test DB");
     }
     container = await new PostgreSqlContainer("postgres:17-alpine").start();
-    // process.env.PG_TEST_CONN = container.getConnectionUri();
+    process.env.PG_TEST_CONN = container.getConnectionUri();
+    const authTestCtx = await auth.$context;
+    authTest = authTestCtx.test;
+    request = supertest(app);
   });
   afterEach(async () => {
     await test_db.clear();
